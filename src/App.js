2c3e9319b562a3f5136e9b3129effa1449b9dba5
yangyi <joe.yangyi@gmail.com>
2016-06-27 10:32:09 +0800

    init commit for effectivce read hot reloading

---
+++ src/App.js
@@ -0,0 +1,10 @@
+"use strict";
+const React = require('react');
+class App extends React.Component {
+    render() {
+        return (React.createElement("span", null, "great"));
+    }
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = App;
+//# sourceMappingURL=App.js.map
